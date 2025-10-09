2d4a3e73b5732ce828d98c0fcdea28856337eda8
Vexachu <pitchblackvorona@gmail.com>
2025-10-08 20:56:54 -0500

    Create firebase-config.example.js

---
+++ assets/firebase-config.example.js
@@ -0,0 +1,13 @@
+// Example Firebase configuration file.
+// Copy this to "firebase-config.js" and fill in your real values.
+// DO NOT commit firebase-config.js itself.
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
